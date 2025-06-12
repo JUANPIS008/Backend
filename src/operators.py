e3012677bd1a623d70962adf47505c00433dff00
Dayana Pulido <144375079+Bygyacqq@users.noreply.github.com>
2025-06-12 13:04:40 -0500

    códgio de listado de horarios para la citas

--- src/operators.py
+++ src/operators.py
@@ -6,3 +6,11 @@ def insertdUsuarios(usuario, contrasena):
     cursor.execute("INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)", (usuario, contrasena))
     connec.commit()
     connec.close()
+    
+def get_horarios_disponibles():
+    con = connection()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM horarios WHERE disponible = 1")
+    resultados = cursor.fetchall()
+    con.close()
+    return resultados
